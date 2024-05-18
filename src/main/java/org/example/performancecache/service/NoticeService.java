cfac687ff86656d86436a765b819ae481a75506e
jaedeokhan <hjaedeok@gmail.com>
2024-05-18 14:03:49 +0900

    first commit

---
+++ src/main/java/org/example/performancecache/service/NoticeService.java
@@ -0,0 +1,17 @@
+package org.example.performancecache.service;
+
+import org.example.performancecache.dto.Notice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public interface NoticeService {
+    List<Notice> getAllNotices();
+
+    List<Notice> findByPage(HttpServletRequest request, int pageNumber);
+
+    List<Notice> findNoticesByDates(LocalDateTime startDate, LocalDateTime endDate);
+
+}
